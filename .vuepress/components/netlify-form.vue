e77d06be0f5a9c38952636b8049a7d749bf114c1
Kevin Duck <kevingduck@gmail.com>
2018-08-26 18:54:55 -0400

    update vue form

--- .vuepress/components/netlify-form.vue
+++ .vuepress/components/netlify-form.vue
@@ -1,55 +1,29 @@
 <template>
-<form netlify>
+<form name="new-website" action="https://formspree.io/tensorwebservices@gmail.com" method="POST" netlify>
+  <ul style="list-style:none">
+    <input type="hidden" name="_next" value="/go.html" />
+    <input type="hidden" name="_subject" value="New request to build a website!" />
+    <input type="text" name="_gotcha" style="display:none" />
+    <p>Your Contact Info:</p>
+    <input type="text" name="_replyto" placeholder="Your email (the link to the new site will be sent to this email address)" />
+    <p>Your Business's Info:</p>
+    <li><input type="text" name="business-name" placeholder="Business Name"></li>
+    <li><input type="text" name="tagline" placeholder="Tagline"></li>
+    <li><input type="text" name="contact-number" placeholder="Business Contact Phone Number"></li>
+    <li><input type="text" pname="contact-email" placeholder="Business Contact Email Address"></li>
+    <li><input type="text" name="business-address" placeholder="Business Address"></li>
+    <li><input type="text" name="specialty" placeholder="Business Specialty"></li>
+    <li><input type="text" name="business-description" placeholder="Business Description"></li>
+    <p>List Your Products/ Services:</p>
+    <li><input type="text" name="pr1" placeholder="Product 1"></li>
+    <li><input type="text" name="pr1-desc" placeholder="Product 1 Description"></li>
+    <li><input type="text" name="pr2" placeholder="Product 2"></li>
+    <li><input type="text" name="pr2-desc" placeholder="Product 2 Description"></li>
+    <li><input type="text" name="pr3" placeholder="Product 3"></li>
+    <li><input type="text" name="pr3-desc" placeholder="Product 3 Description"></li>
+  </ul>
   <p>
-    <label>
-      Your name: <input type="text" name="name" v-model="form.name" />
-    </label>
-  </p>
-  <p>
-    <label>
-      Your Email: <input type="email" name="email" v-model="form.email" />
-    </label>
-  </p>
-  <p>
-    <label>
-      Message: <textarea name="message" v-model="form.message" />
-    </label>
-  </p>
-  <p>
-    <button type="submit" @click.prevent="handleSubmit">Send</button>
+    <button id="form-button" value="Generate site" type="submit">Generate site</button>
   </p>
 </form>
 </template>
-
-
-<script>
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        email: '',
-        message: '',
-      },
-    };
-  },
-  methods: {
-    encode(data) {
-      return Object.keys(data)
-        .map(
-          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
-        )
-        .join('&');
-    },
-    handleSubmit() {
-      fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: this.encode({ 'form-name': 'contact', ...this.form }),
-      })
-        .then(() => alert('Success!'))
-        .catch(error => alert(error));
-    },
-  },
-};
-</script>
--- go.md
+++ go.md
@@ -1,1 +1,8 @@
-<netlify-form/>
+# Success!
+
+<h1>Success! Your site is being generated.</h1>
+
+<h2>Check your email for a link.</h2>
+
+<span>Or check the status of your site by entering your email address:</span>
+<input type="text" placeholder="Email Address">
